017304e823b4cea50047ba2eb8976cfb749149c1
kobi <kobimiz2001@gmail.com>
2022-06-08 04:06:03 +0300

    restructured project and moved to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const { app, BrowserWindow, globalShortcut } = require('electron')
-const path = require('path')
+import { app, BrowserWindow, globalShortcut } from 'electron'
+import * as path from 'path'
 
 const createWindow = () => {
     const win = new BrowserWindow({
